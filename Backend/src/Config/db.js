48a872243b6363844f33d8548de65c1d590fe245
Krishna Naik <intern.krishna@omnicard.in>
2026-05-28 16:50:12 +0530

    Updated Railway database connection

--- Backend/src/Config/db.js
+++ Backend/src/Config/db.js
@@ -6,15 +6,42 @@ const pool = mysql2.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  connectTimeout: 10000
 });
 
 const db = pool.promise();
 
 db.getConnection()
   .then(() => console.log('MySQL Connected Successfully!'))
-  .catch((err) => console.error('MySQL Connection Failed:', err.message));
+  .catch((err) => console.error('MySQL Connection Failed:', err));
 
 module.exports = db;
+
+
+// const mysql2 = require('mysql2');
+// require('dotenv').config();
+
+// const pool = mysql2.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// const db = pool.promise();
+
+// db.getConnection()
+//   .then(() => console.log('MySQL Connected Successfully!'))
+//   .catch((err) => console.error('MySQL Connection Failed:', err.message));
+
+// module.exports = db;
