25fa393b8e606942303eb2a180563d5314eddd04
Krishna Naik <intern.krishna@omnicard.in>
2026-06-04 12:41:45 +0530

    fix:localhost url set

--- Backend/src/Config/cloudinary.js
+++ Backend/src/Config/cloudinary.js
@@ -1,3 +1,4 @@
+// const cloudinary = require("cloudinary").v2;
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
--- Backend/src/Config/db.js
+++ Backend/src/Config/db.js
@@ -1,50 +1,39 @@
-// // const mysql2 = require('mysql2');
-// // require('dotenv').config();
+//localhost mysql connectionoriginal
+//  const mysql2 = require('mysql2');
+//  require('dotenv').config();
 
-// // const pool = mysql2.createPool({
-// //   host: process.env.DB_HOST,
-// //   user: process.env.DB_USER,
-// //   password: process.env.DB_PASSWORD,
-// //   database: process.env.DB_NAME,
-// //   port: process.env.DB_PORT,
-// //   ssl: {
-// //     rejectUnauthorized: false
-// //   },
-// //   waitForConnections: true,
-// //   connectionLimit: 10,
-// //   queueLimit: 0,
-// //   connectTimeout: 10000
-// // });
+//  const pool = mysql2.createPool({
+  
+//    host: process.env.DB_HOST,
+//    user: process.env.DB_USER,
+//    password: process.env.DB_PASSWORD,
+//    database: process.env.DB_NAME,
+//    port: process.env.DB_PORT,
+//    ssl: {
+//      rejectUnauthorized: false
+//    },
+//    waitForConnections: true,
+//    connectionLimit: 10,
+//    queueLimit: 0,
+//   connectTimeout: 10000
+//  });
 
-// // const db = pool.promise();
+//  console.log("DB_HOST:", process.env.DB_HOST);
+// console.log("DB_NAME:", process.env.DB_NAME);
+// console.log("DB_PORT:", process.env.DB_PORT);
 
-// // db.getConnection()
-// //   .then(() => console.log('MySQL Connected Successfully!'))
-// //   .catch((err) => console.error('MySQL Connection Failed:', err));
+//  const db = pool.promise();
 
-// // module.exports = db;
-
-
-// const mysql2 = require('mysql2');
-// require('dotenv').config();
-
-// const pool = mysql2.createPool({
-//   uri: "mysql://root:******@trolley.proxy.rlwy.net:49619/railway"
-// });
-
-// const db = pool.promise();
-
-// db.getConnection()
+//  db.getConnection()
 //   .then(() => console.log('MySQL Connected Successfully!'))
-//   .catch((err) => console.error('MySQL Connection Failed:', err.message));
+//    .catch((err) => console.error('MySQL Connection Failed:', err));
 
-// module.exports = db;
+//  module.exports = db;
 
-
-
-
+// localhost mysql upside is correct database
 
 
+//Mysql railway server connection
 const mysql2 = require('mysql2');
 
 const pool = mysql2.createPool(
